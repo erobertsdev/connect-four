c797a396b4bb18fb775dee86ccf399aa521d0dbe
Elias Roberts <55095594+erobertsdev@users.noreply.github.com>
2020-02-08 16:25:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Connect Four
+
+The most original project you've ever seen.
+Done as a bootcamp assignment.
+The code ended up being surprisingly convoluted. I'll refactor it at some point.
+Maybe. 
+Probably not.
---
+++ src/js/index.js
@@ -0,0 +1,140 @@
+import { board } from './views/board.js';
+
+const init = (() => {
+	board.gameButton.textContent = 'START';
+	board.gameButton.addEventListener('click', () => {
+		board.gameOver = false;
+		board.player = 'blue';
+		board.gameButton.style.display = 'none';
+		board.gameStatus.textContent = `${board.player}'s Turn!`.toUpperCase();
+		board.gameStatus.classList.add(`${board.player}`);
+	});
+})();
+
+for (let square of board.squares) {
+	square.addEventListener('click', () => {
+		let row = square.id.charAt(0);
+		let col = square.id.charAt(2);
+		if (!board.gameOver) {
+			// Checks if column is full
+			if (eval(`board.col${col}`).position !== -1) {
+				if (board.player === 'blue') {
+					board.boardArr[eval(`board.col${col}`).position][col] = 'blue';
+					eval(`board.col${col}.arr`)[eval(`board.col${col}.position`)] = 'blue';
+					eval(`board.col${col}.dom`)[eval(`board.col${col}.position`)].classList.add('blue');
+					if (eval(`board.col${col}`).position >= 0) {
+						eval(`board.col${col}`).position -= 1;
+					}
+					checkForWin(board.player, col);
+					if (!board.gameOver) {
+						changePlayer();
+					}
+				} else {
+					board.boardArr[eval(`board.col${col}`).position][col] = 'red';
+					eval(`board.col${col}.arr`)[eval(`board.col${col}.position`)] = 'red';
+					eval(`board.col${col}.dom`)[eval(`board.col${col}.position`)].classList.add('red');
+					if (eval(`board.col${col}`).position >= 0) {
+						eval(`board.col${col}`).position -= 1;
+					}
+					checkForWin(board.player, col);
+					if (!board.gameOver) {
+						changePlayer();
+					}
+				}
+			} else {
+				board.gameStatus.textContent = 'Column full, try again.'.toUpperCase();
+			}
+		}
+	});
+	square.addEventListener('mouseover', () => {
+		let col = square.id.charAt(2);
+		columnGlow(col);
+	});
+	square.addEventListener('mouseout', () => {
+		let col = square.id.charAt(2);
+		columnGlow(col);
+	});
+}
+
+const columnGlow = (num) => {
+	let cols = eval(`board.col${num}.dom`);
+	cols.forEach((col) => col.classList.toggle('col-hover'));
+};
+
+const checkForWin = (player, column) => {
+	isBoardFull();
+	let count = 0;
+	// Checks for win vertically
+	eval(`board.col${column}.arr`).forEach((el) => {
+		if (el === player) {
+			count++;
+			if (count === 4) {
+				board.gameStatus.textContent = `${player} wins!`.toUpperCase();
+				board.gameOver = true;
+				board.gameButton.style.display = 'inherit';
+				board.gameButton.textContent = 'NEW GAME';
+				board.gameButton.addEventListener('click', () => {
+					location.reload();
+				});
+				return;
+			}
+		} else {
+			count = 0;
+		}
+	});
+	// Checks for win horizontally
+	for (let i = 0; i <= 4; i++) {
+		for (let x = 0; x <= 5; x++) {
+			if (board.boardArr[i][x] === player) {
+				count++;
+				if (count === 4) {
+					board.gameStatus.textContent = `${player} wins!`.toUpperCase();
+					board.gameOver = true;
+					board.gameButton.style.display = 'inherit';
+					board.gameButton.textContent = 'NEW GAME';
+					board.gameButton.addEventListener('click', () => {
+						location.reload();
+					});
+					return;
+				}
+			} else {
+				count = 0;
+			}
+		}
+	}
+};
+
+const changePlayer = () => {
+	if (board.player === 'blue') {
+		board.player = 'red';
+		board.gameStatus.textContent = `${board.player}'s Turn!`.toUpperCase();
+		board.gameStatus.classList.remove('blue');
+		board.gameStatus.classList.add(`${board.player}`);
+	} else {
+		board.player = 'blue';
+		board.gameStatus.textContent = `${board.player}'s Turn!`.toUpperCase();
+		board.gameStatus.classList.remove('red');
+		board.gameStatus.classList.add(`${board.player}`);
+	}
+};
+
+const isBoardFull = () => {
+	let count = 0;
+	for (let i = 0; i <= 4; i++) {
+		for (let x = 0; x <= 5; x++) {
+			if (board.boardArr[i][x]) {
+				count++;
+				if (count === 30) {
+					board.gameStatus.textContent = `GAME OVER. NO WINNER.`.toUpperCase();
+					board.gameOver = true;
+					board.gameButton.style.display = 'inherit';
+					board.gameButton.textContent = 'NEW GAME';
+					board.gameButton.addEventListener('click', () => {
+						location.reload();
+					});
+					return;
+				}
+			}
+		}
+	}
+};
